c132e64d56e005d59bea4e032940b9bf0549a99a
Alex <31377763+AlexH893@users.noreply.github.com>
2021-08-08 23:34:29 -0500

    add person.interface.ts

---
+++ week-1/ts-examples/person.interface.ts
@@ -0,0 +1,12 @@
+/**
+ * Title: person.interface.ts
+ * Author: Professor Krasso
+ * Modified: Alex Haefner
+ * Date: 8 August 2021
+ * Description: Person interface
+ */
+
+export interface IPerson {
+    firstName: string;
+    lastName: string;
+}
