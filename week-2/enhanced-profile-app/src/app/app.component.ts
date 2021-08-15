7e3981ed23c1d75826755227f48c30827b981138
Alex <31377763+AlexH893@users.noreply.github.com>
2021-08-15 16:32:44 -0500

    add enhanced profile app with data binding

---
+++ week-2/enhanced-profile-app/src/app/app.component.ts
@@ -0,0 +1,30 @@
+/*
+ * Title: app.component.ts
+ * Author: Alex Haefner
+ * Date: 15 August 2021
+ * Description: The app.component ts file for my-app
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment: string = 'Assignment 2.3 - Data Binding';
+
+  //Define isLoggedIn variable w/ Boolean data type, assign it default value of true
+  isLoggedIn: boolean = true;
+
+
+
+
+
+
+
+  }
+
+
+var isLoggedIn = true
